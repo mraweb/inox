675883dacb722fba123f0b301eaed544baeadd50
marcelo@mraweb.com.br <marcelo@mraweb.com.br>
2014-06-19 15:17:03 -0300

    Atualização Grunt

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,1 @@
-$(document).ready(function(){
-	// link com target _blank
-    $("a[rel=external]").attr('target','_blank');
-
-    // desabilita skype
-    $('head').append('<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />');
-
-    // tira padding-right de lista de links na home
-    $(".alinha ul li:nth-child(3n), .box-tanques ul li:nth-child(4n)").css("padding-right", "0");
-
-    // colorbox
-    $(".group1, .group2").colorbox();
-});
+$(document).ready(function(){$("a[rel=external]").attr("target","_blank");$("head").append('<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />');$(".alinha ul li:nth-child(3n), .box-tanques ul li:nth-child(4n)").css("padding-right","0");$(".group1, .group2").colorbox()});
